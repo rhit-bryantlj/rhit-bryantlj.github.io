c832686e83ef0e6b2368f23c2cadcfde6ee91809
rhit-bryantlj <bryantlj@rose-hulman.edu>
2021-09-28 12:08:46 -0400

    replaced the main.js with the template

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,23 @@
-let resumeButton = document.getElementById("#resumeDownload");
-console.log(resumeButton);
+/**
+ * @fileoverview
+ * Provides the JavaScript interactions for all pages.
+ *
+ * @author 
+ * Logan Bryant
+ */
+
+/** namespace. */
+var rhit = rhit || {};
+
+/** function and class syntax examples */
+rhit.functionName = function () {
+	/** function body */
+};
+
+/* Main */
+/** function and class syntax examples */
+rhit.main = function () {
+	console.log("Ready");
+};
+
+rhit.main();
