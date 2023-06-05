146fd75b1e6f02d0a13d61e1cb8c32fb204390b8
rhit-bryantlj <bryantlj@rose-hulman.edu>
2023-06-04 21:49:27 -0400

    overhaul of site coloring, added the transition nav, and a blank set for the project page

--- scripts/main.js
+++ scripts/main.js
@@ -8,16 +8,29 @@
 
 /** namespace. */
 var rhit = rhit || {};
+var navOpened = false;
 
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
-    let resumeButton = document.querySelector("#resumeDownload");
-    
-    resumeButton.onclick = (event) =>{
-        
-    };
+};
+
+function navClicked() { 
+    let navbar = document.querySelector("#myNav");
+    let navMenu = document.querySelector("#navMenuButton");
+    if(!navOpened){
+        if(screen.width > 700)
+            navbar.style.height = "8em"
+        else    
+            navbar.style.height = "2em"
+        navMenu.innerHTML= "☰ Close"
+        navOpened = true;
+    } else {
+        navbar.style.height = "0";
+        navMenu.innerHTML= "☰ Menu"
+        navOpened = false;
+    }
 };
 
 rhit.main();
